cb957c15dd4f9b5b3c020fbc621be3a1e9062d0d
Gabriel Guerra <gfranklings@gmail.com>
2022-11-24 16:00:53 -0400

    using gdm over lightdm

--- hosts/ultra/configuration.nix
+++ hosts/ultra/configuration.nix
@@ -55,7 +55,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 
   services.xserver.windowManager.awesome = {
     enable = true;
