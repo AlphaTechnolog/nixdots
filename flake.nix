b69b606bf0a38e7273ca34c8e3c72e3ef240bb5f
Gabriel Guerra <gfranklings@gmail.com>
2022-11-24 15:45:34 -0400

    fixed tabs issues

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  	home-manager = {
+    home-manager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
