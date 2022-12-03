276b98b21cd388ebdf8d6ed154436a7246304417
Gabriel Guerra <gfranklings@gmail.com>
2022-12-03 19:39:08 -0400

    reenabled titlebars on awesomewm

--- users/alpha/cfg/awesome/ui/init.lua
+++ users/alpha/cfg/awesome/ui/init.lua
@@ -2,6 +2,6 @@ require 'ui.notifications'
 require 'ui.wallpaper'
 require 'ui.menu'
 require 'ui.bar'
--- require 'ui.titlebars'
+require 'ui.titlebars'
 require "ui.windows_switcher"
 -- require 'ui.flash_focus'
