15ebac4a4805e49f5533a59d845d127402ee19d1
Gabriel Guerra <gfranklings@gmail.com>
2022-12-03 14:00:01 -0400

    autoclosing nvim-tree when opening a file

--- users/alpha/cfg/nvim/lua/autocmds.lua
+++ users/alpha/cfg/nvim/lua/autocmds.lua
@@ -1,1 +1,11 @@
 -- vim.cmd [[ autocmd VimLeave * set guicursor=n:ver10 ]]
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+  nested = true,
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  command = "if bufname() != 'NvimTree_' . tabpagenr() | NvimTreeClose | endif",
+  nested = true,
+})
