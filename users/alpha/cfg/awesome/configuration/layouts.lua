26bb16fce137d6f4f0e4a76f785c9bc00cdd782a
Gabriel Guerra <gfranklings@gmail.com>
2022-12-04 12:00:07 -0400

    using floating layout as the default one

--- users/alpha/cfg/awesome/configuration/layouts.lua
+++ users/alpha/cfg/awesome/configuration/layouts.lua
@@ -4,8 +4,8 @@ local awful = require "awful"
 local function set_layouts()
     tag.connect_signal("request::default_layouts", function ()
         awful.layout.append_default_layouts {
-            awful.layout.suit.spiral.dwindle,
             awful.layout.suit.floating,
+            awful.layout.suit.spiral.dwindle,
             awful.layout.suit.max,
         }
     end)
