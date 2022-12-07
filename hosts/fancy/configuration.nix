88f4a5fde15ff4bc3017e99cd4ab4aa965df0afc
Gabriel Guerra <gfranklings@gmail.com>
2022-12-07 13:44:36 -0400

    fixed hostname for wayland config

--- hosts/fancy/configuration.nix
+++ hosts/fancy/configuration.nix
@@ -35,7 +35,7 @@ in {
   # unstable kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "ultra"; # Define your hostname.
+  networking.hostName = "fancy"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
