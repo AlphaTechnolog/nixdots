56b163a428ab2f873b4a80c37f3642c7ec1f9277
Gabriel Guerra <gfranklings@gmail.com>
2022-12-12 14:37:37 -0400

    nvim: set cmdheight=0

--- users/alpha/cfg/nvim/lua/config/options.lua
+++ users/alpha/cfg/nvim/lua/config/options.lua
@@ -11,6 +11,7 @@ opt.laststatus = 3
 opt.cursorline = false
 opt.wrap = false
 opt.guifont = { "JetBrainsMono Nerd Font", ":h8" }
+opt.cmdheight = 0
 
 opt.shortmess = opt.shortmess + {
   c = true, -- Do not show completion messages in command line
