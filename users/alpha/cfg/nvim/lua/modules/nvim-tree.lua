a4ed361a2fee72e8a7de19cc92e6cf8a0f66b3f7
Gabriel Guerra <gfranklings@gmail.com>
2022-12-17 02:57:50 -0400

    nvim: make nvim config work at installation

--- users/alpha/cfg/nvim/lua/modules/nvim-tree.lua
+++ users/alpha/cfg/nvim/lua/modules/nvim-tree.lua
@@ -7,3 +7,13 @@ end
 ntree.setup {}
 
 vim.api.nvim_set_keymap('n', '<C-n>', '<cmd>NvimTreeToggle<cr>', { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+  nested = true,
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  command = "if bufname() != 'NvimTree_' . tabpagenr() | NvimTreeClose | endif",
+  nested = true,
+})
