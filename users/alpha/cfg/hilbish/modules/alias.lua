b2c953c187e6851db1a15cf868f984acc9a45098
Gabriel Guerra <gfranklings@gmail.com>
2022-12-23 19:23:47 -0400

    awesome: changed wallpaper & added more reset aliases onto hilbish config

--- users/alpha/cfg/hilbish/modules/alias.lua
+++ users/alpha/cfg/hilbish/modules/alias.lua
@@ -5,8 +5,8 @@ util.process_table(hilbish.alias, {
   ls = "exa --icons",
   la = "ls -la",
   tree = "ls --tree",
-  vim = "nvim",
-  vi = "nvim",
+  vim = "nvim && exec hilbish -S -l", -- restart hilbish
+  vi = "nvim && exec hilbish -S -l", -- restart hilbish
   cat = "bat",
   reset = "exec hilbish -S -l" -- replaces current shell into a new one
 })
