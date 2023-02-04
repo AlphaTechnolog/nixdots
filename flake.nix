7beead61322205b014fdded2480cb3963fa2ebad
Gabriel Guerra <gfranklings@gmail.com>
2023-02-03 23:12:28 -0400

    chore: beautified the flake.nix

--- flake.nix
+++ flake.nix
@@ -46,61 +46,64 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixpkgs-f2k, luaFormatter, ... }@inputs:
-    let
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, home-manager, nixpkgs-f2k, luaFormatter, ... }@inputs: let
+    system = "x86_64-linux";
 
-      pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = nixpkgs.legacyPackages.${system};
 
-      config = {
-        system = system;
-        allowUnfree = true;
-      };
-
-      lib = nixpkgs.lib;
-
-      overlays = with inputs; [
-        (final: _:
-          let
-            inherit (final) system;
-          in {
-            neovim-nightly = neovim.packages."${system}".neovim;
-            st = pkgs.callPackage ./pkgs/st.nix {
-              src = st;
-              conf = import ./users/alpha/programs/st/config.nix;
-            };
-          } // (with nixpkgs-f2k.packages.${system}; {
-            awesome = awesome-git;
-            picom = picom-git;
-          }) // {
-            luaFormatter-src = luaFormatter;
-            rxfetch-bin = pkgs.callPackage ./pkgs/rxfetch { src = inputs.rxfetch; };
-            lunarvim-src = inputs.lunarvim;
-            lunarvim-wrapper = pkgs.writeScriptBin "lvim" ''
-              #!${pkgs.bash}/bin/bash
-              exec -a lvim nvim -u "$HOME/.local/share/lunarvim/lvim/init.lua" "$@"
-            '';
-            sf-mono-liga-bin = pkgs.callPackage ./pkgs/sfmono-nf.nix {
-              src = inputs.sf-mono-liga-src;
-            };
-          }
-        )
-        (final: prev: {
-          phosphor-icons = prev.callPackage ./pkgs/phosphor.nix {};
-          luaFormatter = prev.callPackage ./pkgs/luaFormatter.nix {
-            src = prev.luaFormatter-src;
-            version = "999-master";
-          };
-        })
-        neovim-nightly.overlay
-        nixpkgs-f2k.overlays.default
-        nur.overlay
-      ];
-    in {
-      nixosConfigurations = {
-        ultra = import ./hosts/ultra {
-          inherit config nixpkgs overlays inputs system home-manager;
-        };
-      };
+    config = {
+      system = system;
+      allowUnfree = true;
     };
+
+    lib = nixpkgs.lib;
+
+    overlays = with inputs; [
+      (final: _:
+        let
+          inherit (final) system;
+        in {
+          neovim-nightly = neovim.packages."${system}".neovim;
+          st = pkgs.callPackage ./pkgs/st.nix {
+            src = st;
+            conf = import ./users/alpha/programs/st/config.nix;
+          };
+        } // (with nixpkgs-f2k.packages.${system}; {
+          awesome = awesome-git;
+          picom = picom-git;
+        }) // {
+          luaFormatter-src = luaFormatter;
+          rxfetch-bin = pkgs.callPackage ./pkgs/rxfetch { src = inputs.rxfetch; };
+          lunarvim-src = inputs.lunarvim;
+          lunarvim-wrapper = pkgs.writeScriptBin "lvim" ''
+            #!${pkgs.bash}/bin/bash
+            exec -a lvim nvim -u "$HOME/.local/share/lunarvim/lvim/init.lua" "$@"
+          '';
+          sf-mono-liga-bin = pkgs.callPackage ./pkgs/sfmono-nf.nix {
+            src = inputs.sf-mono-liga-src;
+          };
+        }
+      )
+      (final: prev: {
+        phosphor-icons = prev.callPackage ./pkgs/phosphor.nix {};
+        luaFormatter = prev.callPackage ./pkgs/luaFormatter.nix {
+          src = prev.luaFormatter-src;
+          version = "999-master";
+        };
+      })
+      neovim-nightly.overlay
+      nixpkgs-f2k.overlays.default
+      nur.overlay
+    ];
+  in {
+    nixosConfigurations.ultra = import ./hosts/ultra {
+      inherit
+        nixpkgs
+        config
+        overlays
+        inputs
+        system
+        home-manager;
+    };
+  };
 }
